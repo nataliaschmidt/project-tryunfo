32cb74147b4e0a8820370de6fb582bd02f37e104
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-06 17:01:22 -0300

    Requisito 05

--- src/App.js
+++ src/App.js
@@ -12,8 +12,46 @@ class App extends React.Component {
     cardImage: '',
     cardRare: 'normal',
     cardTrunfo: false,
-    isSaveButtonDisabled: false,
-    // Ativado para testes
+    isSaveButtonDisabled: true,
+  };
+  // Campos nome, descrição, imagem e raridade devem conter alguma informação
+  // Soma do valor dos 3 atributos não pode ser maior do que 210
+  // Cada um dos 3 atributos pode ter no máximo 90 pontos e não podem receber valores negaticos
+
+  validadeTextForm = () => {
+    const { cardName, cardDescription, cardImage, cardRare } = this.state;
+    const textName = cardName.length > 0;
+    const textDescription = cardDescription.length > 0;
+    const textImage = cardImage.length > 0;
+    const textRare = cardRare.length > 0;
+
+    return textName && textDescription && textImage && textRare;
+  };
+
+  validateSumAndMinMaxAttr = () => {
+    const SUM = 210;
+    const MAX_LENGTH = 90;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const sumAttr = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= SUM;
+    const maxMinValue1 = cardAttr1 >= 0 && cardAttr1 <= MAX_LENGTH;
+    const maxMinValue2 = cardAttr2 >= 0 && cardAttr2 <= MAX_LENGTH;
+    const maxMinValue3 = cardAttr3 >= 0 && cardAttr3 <= MAX_LENGTH;
+    return sumAttr && maxMinValue1 && maxMinValue2 && maxMinValue3;
+  };
+
+  validateForm = () => {
+    const validateAttr = this.validateSumAndMinMaxAttr();
+    console.log(validateAttr);
+    const validateText = this.validadeTextForm();
+    if (validateAttr && validateText) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
   };
 
   onInputChange = ({ target }) => {
@@ -22,7 +60,7 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
-    });
+    }, this.validateForm);
   };
 
   onSaveButtonClick = (event) => {
