8f6a55f1792a4d66bb44e04d242746a3e587c245
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-06 17:59:26 -0300

    Requisito 08

--- src/App.js
+++ src/App.js
@@ -108,6 +108,7 @@ class App extends React.Component {
       cardImage,
       cardRare,
       isSaveButtonDisabled,
+      cards,
     } = this.state;
 
     return (
@@ -139,6 +140,24 @@ class App extends React.Component {
             cardRare={ cardRare }
             cardTrunfo={ cardTrunfo }
           />
+        </section>
+        <section>
+          <h2>TODAS AS CARTAS</h2>
+          {
+            cards.map((card) => (
+              <Card
+                key={ card.cardName }
+                cardName={ card.cardName }
+                cardDescription={ card.cardDescription }
+                cardAttr1={ card.cardAttr1 }
+                cardAttr2={ card.cardAttr2 }
+                cardAttr3={ card.cardAttr3 }
+                cardImage={ card.cardImage }
+                cardRare={ card.cardRare }
+                cardTrunfo={ card.cardTrunfo }
+              />
+            ))
+          }
         </section>
       </>
     );
